a9bb38ac732edee88e370e13c82ad07bed99a6bb
DanielBaquero28 <835@holbertonschool.com>
2020-05-03 16:17:29 -0500

    [Fixed]: Put a function into first exercise

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -175,28 +175,3 @@ binary_tree_t *parent_insert(binary_tree_t *tree)
 	return (tree);
 }
 
-/**
- * binary_tree_height - Finds the height of a tree
- * @tree: Tree from which the height will be found
- * Return: Depth of the tree
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t hleft_sub, hright_sub;
-
-	if (!tree)
-		return (0);
-
-	hleft_sub = 0;
-	hright_sub = 0;
-
-	if (tree->left)
-		hleft_sub = binary_tree_height(tree->left) + 1;
-	if (tree->right)
-		hright_sub = binary_tree_height(tree->right) + 1;
-
-	if (hleft_sub > hright_sub)
-		return (hleft_sub);
-	return (hright_sub);
-}
