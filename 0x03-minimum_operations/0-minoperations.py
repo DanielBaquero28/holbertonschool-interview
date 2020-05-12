c809e00ce92271bb69e297aea58257ecb1a3b027
DanielBaquero28 <835@holbertonschool.com>
2020-05-12 12:55:09 -0500

    [Feature]: Got it !

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """ Finds the minimum number of operations to get n H """
+    op = 0
+    char = 1
+    paste_char = 0
+    flag = 0
+
+    if (n <= 1):
+        return (0)
+    while ((char * 2) <= n):
+        op += 2
+        paste_char = char
+        char *= 2
+        if (char == n):
+            flag = 1
+            break
+    while (char < n):
+        if ((char + paste_char) <= n):
+            op += 1
+            char += paste_char
+            if (char == n):
+                flag = 1
+                break
+        if ((char + paste_char) > n):
+            flag = 0
+            break
+        """ if (char == n):
+            flag = 1
+            break """
+    if (flag == 1):
+        return (op)
+    return (0)
