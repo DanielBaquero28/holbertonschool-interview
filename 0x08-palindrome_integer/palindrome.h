c9d9c80eb1056efd960e718144bdceb72c62b5fa
DanielBaquero28 <835@holbertonschool.com>
2020-06-14 12:09:00 -0500

    [Feature]: Adding header and gitignore files

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,11 @@
+#ifndef H_PALINDROME_INTEGER
+#define H_PALINDROME_INTEGER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/** Prototypes */
+int is_palindrome(unsigned long n);
+
+#endif
