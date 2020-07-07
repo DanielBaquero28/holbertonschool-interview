580ef76ce25cffbd4b55611f1e8a87227eecd1a0
DanielBaquero28 <835@holbertonschool.com>
2020-07-07 14:07:58 -0500

    [Feature]: Testing v0

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,35 @@
+#include "menger.h"
+
+/**
+ * menger - Prints a 2D Menger Sponge.
+ * @level: Level of @menger
+ * Return: Nothing
+ */
+
+void menger(int level)
+{
+	int size_sponge, tmp_i, tmp_j;
+	char c;
+
+	size_sponge = pow(3, level);
+	for (int i = 0; i < size_sponge; i++)
+	{
+		for (int j = 0; j < size_sponge; j++)
+		{
+			c = '#';
+			tmp_i = i;
+			tmp_j = j;
+			while (tmp_i > 0)
+			{
+				if (tmp_i % 3 == 1 && tmp_j % 3 == 1)
+				{
+					c = ' ';
+				}
+				tmp_i /= 3;
+				tmp_j /= 3;
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+	}
+}
