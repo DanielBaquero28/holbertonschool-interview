aa9c3d2f9c71372ac4cc4331e829787df2b575f6
DanielBaquero28 <835@holbertonschool.com>
2020-10-12 17:55:06 +0000

    [Feature]: Allowed only 3 iterations over whatever length of the matrix

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,7 +6,7 @@ def rotate_2d_matrix(matrix):
     """ The matrix is edited in-place. It doesn't return anything. """
 
     l = len(matrix)
-    for z in range(l):
+    for z in range(3):
         for i in range(0, int(l/2)):
             for j in range(i, l - 1 - i):
                 temp = matrix[i][j]
